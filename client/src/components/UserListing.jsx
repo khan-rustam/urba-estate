48a37a3e19b5a11d22752e16fd9579f5f988a7cc
Rustam Khan <khanrustam2809@gmail.com>
2024-05-17 22:08:16 +0530

    Update UserListing.jsx

--- client/src/components/UserListing.jsx
+++ client/src/components/UserListing.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import  { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function UserListing() {
   const { currentUser } = useSelector((state) => state.user);
@@ -10,19 +10,19 @@ export default function UserListing() {
   const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: "DELETE",
+        method: 'DELETE',
       });
       const data = await res.json();
       if (data.success === false) {
         console.log(data.message);
-        
+
         return;
       }
 
       setUserListings((prev) =>
         prev.filter((listing) => listing._id !== listingId)
       );
-      alert("Listing deleted successfully");
+      alert('Listing deleted successfully');
     } catch (error) {
       console.log(error.message);
     }
@@ -49,49 +49,64 @@ export default function UserListing() {
     handleShowListings();
   }, []);
   return (
-    <section className="py-10 lg:py-20 bg-stone-100">
-      <div className="max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
-        <h1 className="md:text-3xl text-3xl font-semibold border-b p-3 text-slate-700">
+    <section className='py-10 lg:py-20 bg-stone-100'>
+      <div className='max-w-6xl py-4 mx-auto lg:py-6 md:px-6'>
+        <h1 className='md:text-3xl text-3xl font-semibold border-b p-3 text-slate-700'>
           Your Listings:
         </h1>
-        <p className="text-red-700 mt-5">
-          {showListingsError ? "Error showing listings" : ""}
+        <p className='text-red-700 mt-5'>
+          {showListingsError ? 'Error showing listings' : ''}
         </p>
 
-        {userListings && userListings.length > 0 && (
-          <div className="flex flex-col gap-4">
+        {userListings && userListings.length > 0 ? (
+          <div className='flex flex-col gap-4'>
             {userListings.map((listing) => (
               <div
                 key={listing._id}
-                className="border rounded-lg p-3 flex justify-between items-center gap-4"
+                className='border rounded-lg p-3 flex justify-between items-center gap-4'
               >
                 <Link to={`/listing/${listing._id}`}>
                   <img
                     src={listing.imageUrls[0]}
-                    alt="listing cover"
-                    className="h-20 w-20 rounded-md object-contain"
+                    alt='listing cover'
+                    className='h-20 w-20 rounded-md object-contain'
                   />
                 </Link>
                 <Link
-                  className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                  className='text-slate-700 font-semibold  hover:underline truncate flex-1'
                   to={`/listing/${listing._id}`}
                 >
                   <p>{listing.name}</p>
                 </Link>
 
-                <div className="flex flex-col item-center">
+                <div className='flex flex-col item-center'>
                   <button
                     onClick={() => handleListingDelete(listing._id)}
-                    className="text-red-700 uppercase"
+                    className='text-red-700 uppercase'
                   >
                     Delete
                   </button>
                   <Link to={`/update-listing/${listing._id}`}>
-                    <button className="text-green-700 uppercase">Edit</button>
+                    <button className='text-green-700 uppercase'>Edit</button>
                   </Link>
                 </div>
               </div>
             ))}
+          </div>
+        ) : (
+          <div className='text-center pt-11'>
+            <h1 className='md:text-3xl text-3xl font-semibold p-3  text-slate-700'>
+              You Dont have any listing yet.
+            </h1>
+            <p>
+              Create your first listing now?{' '}
+              <Link
+                to={'/create-listing'}
+                className=' text-blue-500 underline uppercase text-sm cursor-pointer'
+              >
+                Click here
+              </Link>
+            </p>
           </div>
         )}
       </div>
